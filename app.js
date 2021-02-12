25819a03603a78b9b21e14ca9e44aea42c762188
RemoteSoul-Hub <yahya.tahoune@gmail.com>
2021-02-12 01:17:04 +0100

    Pseudo code for app.js, base structure of page on HTML / CSS

---
+++ app.js
@@ -0,0 +1,20 @@
+let task = document.getElementById('task');
+let submit = document.getElementById('submit');
+let clear = document.getElementById('clear');
+
+/// Script that creates the UL / adds Li's after detecting a click on submit 
+
+
+
+// script that detects a click on submit 
+
+
+// script that captures the text typed and sends it to script #1
+
+
+// Script that clears the whole list (clear all button)
+//Listen to event of click then proceed.
+
+
+
+
